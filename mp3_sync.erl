3ccac42373015d32b1273aba0a6de107554c3313
junjiemars <junjiemars@gmail.com>
2014-04-04 10:55:27 +0800

    modify makefile & mp3_sync

--- mp3_sync.erl
+++ mp3_sync.erl
@@ -20,22 +20,22 @@
 %% are synced - require three in a row to achieve sync
 
 find_sync(Bin, N) ->
-    case is_header(N, Bin) of
-	{ok, Len1, _} ->
-	    case is_header(N + Len1, Bin) of
-		{ok, Len2, _} ->
-		    case is_header(N + Len1 + Len2, Bin) of
-			{ok, _, _} ->
-			    {ok, N};
+  case is_header(N, Bin) of
+		{ok, Len1, _} ->
+		case is_header(N + Len1, Bin) of
+			{ok, Len2, _} ->
+	    	case is_header(N + Len1 + Len2, Bin) of
+					{ok, _, _} ->
+		   			{ok, N};
+					error ->
+		   			find_sync(Bin, N+1)
+	    	end;
 			error ->
-			    find_sync(Bin, N+1)
-		    end;
+		   	find_sync(Bin, N+1)
+		end;
 		error ->
-		    find_sync(Bin, N+1)
-	    end;
-	error ->
 	    find_sync(Bin, N+1)
-    end.
+	end.
 %% is_header(N, Bin) -> {ok, FrameLength, Info} | error.
 
 is_header(N, Bin) ->
